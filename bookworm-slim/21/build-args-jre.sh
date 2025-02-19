28745dc9f9febd3ac38496352b535b9b96a51bd5
unserori <pd.ai.374005475@gmail.com>
2025-02-19 22:32:48 +0900

    [update]引数として不要な部分を修正

--- bookworm-slim/21/build-args-jre.sh
+++ bookworm-slim/21/build-args-jre.sh
@@ -3,7 +3,5 @@
 # 空白を含む文字列のため"で囲むこと、また改行でわかりやすく記述している
 
 args="
-  --build-arg JAVA_BOOT_JDK=eclipse-temurin:20.0.2_9-jdk \
-  --build-arg JAVA_SOURCE_TAG=jdk-21-ga \
   --build-arg ARTIFACT_PATH=jre \
 "
